6cec5e6132a2dbd536e7648dea479854f0455510
JongBok5057 <ljb0362@gmail.com>
2021-06-08 16:59:43 +0900

    youtube title

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from'react';
 import './app.css';
-import './components/habit';
+import VideoList from './components/video_list/video_list';
+
 
 function App() {
-  return (
-    <Habit />
-  );
-}
+  const [videos,setVideos] = useState([]);
+
+  useEffect(() => {
+    const requestOptions = {
+      method: 'GET',
+      redirect: 'follow'
+    };
+    
+    fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyCKNcesNT5JIV9kBRtugiDmjkyOOM3-Zgg", requestOptions)
+      .then(response => response.json())
+      .then(result => setVideos(result.items))
+      .catch(error => console.log('error', error));
+  } , []);
+  return <VideoList videos = {videos} />;
+};
 
 export default App;
